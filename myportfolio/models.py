6f00025f8385b39f3488a5bdf99e3728f661f5de
Isaac-kiptoo-kilimo <isaac.kilimo@student.moringaschool.com>
2022-05-27 06:57:26 +0300

    feature:added the new projects

--- myportfolio/models.py
+++ myportfolio/models.py
@@ -7,13 +7,14 @@ class Editor(models.Model):
     lastname=models.CharField(max_length=200)
     email=models.EmailField()
     phonenumber=models.CharField(max_length = 10,blank =True)
+    pro_image=models.ImageField(upload_to='index/',null=True)
 
     def __str__(self):
         return self.firstname
 
 class Project(models.Model):
     title=models.CharField(max_length=50)
-    description=models.CharField(max_length=1000)
+    description=models.TextField(max_length=1000)
     date_posted=models.DateTimeField(auto_now_add=True)
     project_image=models.ImageField(upload_to='project/',null=True)
     editor=models.ForeignKey(Editor,on_delete=models.PROTECT)
--- myportfolio/views.py
+++ myportfolio/views.py
@@ -1,5 +1,22 @@
 from django.shortcuts import render
+from django.http import Http404
+from .models import Editor, Project
 
 # Create your views here.
 def index(request):
     return render(request,'pages/index.html')
+
+def add_project(request):
+    try:
+        project = Project.objects.all()
+    except Project.DoesNotExist:
+        raise Http404()
+    return render(request,"pages/index.html", {"projects":project})
+
+def add_editor(request):
+    try:
+        editor = Editor.objects.all()
+    except Editor.DoesNotExist:
+        raise Http404()
+    return render(request,"pages/index.html", {"editors":editor})
+    
